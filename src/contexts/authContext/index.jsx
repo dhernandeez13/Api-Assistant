ea6ad2ca2392492eb97b37d97e097fe39e5d43d5
dhg0003 <danielpsnher@gmail.com>
2025-06-26 23:02:18 +0200

    Login Classic, Email & Github with Firebase

---
+++ src/contexts/authContext/index.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useContext } from "react";
+import { auth } from "../../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = React.createContext();
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = React.useState(null);
+  const [userLoggedIn, setUserLoggedIn] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+    return unsubscribe;
+  }, []);
+
+  async function initializeUser(user) {
+    if (user) {
+      setCurrentUser({ ...user });
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
+    }
+    setLoading(false);
+  }
+  const value = {
+    currentUser,
+    userLoggedIn,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDV4szjWrY0uz5Z2h6KMOxy5BWeAH6bYNg",
+  authDomain: "apiassistant-33adb.firebaseapp.com",
+  projectId: "apiassistant-33adb",
+  storageBucket: "apiassistant-33adb.firebasestorage.app",
+  messagingSenderId: "970598283727",
+  appId: "1:970598283727:web:22c673109fab8fe9f83aba",
+  measurementId: "G-JWCQ98DS32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { app, analytics, auth };
